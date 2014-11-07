ded43a3d89f2202cc118fd0d58e76a4b4cd85341
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2014-11-07 22:47:44 +0400

    initial import

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '2.1.2'
+
+gem 'thin'
+gem 'sinatra', '~> 1.4.5', require: 'sinatra/base'
+gem 'pocket-ruby', '~> 0.0.5', require: 'pocket'
---
+++ app.rb
@@ -0,0 +1,68 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default)
+
+class App < Sinatra::Base
+  enable :logging
+  enable :sessions
+
+  CALLBACK_URL = "http://localhost:#{ENV['PORT']}/oauth/callback"
+
+  Pocket.configure do |config|
+    config.consumer_key = ENV['CONSUMER_KEY']
+  end
+
+  get '/reset' do
+    logger.info 'GET /reset'
+    session.clear
+  end
+
+  get '/' do
+    logger.info 'GET /'
+    logger.info "session: #{session.inspect}"
+
+    if session[:access_token]
+      '
+  <a href="/add?url=http://getpocket.com">Add Pocket Homepage</a>
+  <a href="/retrieve">Retrieve single item</a>
+      '
+    else
+      '<a href="/oauth/connect">Connect with Pocket</a>'
+    end
+  end
+
+  get '/oauth/connect' do
+    logger.info 'OAUTH CONNECT'
+    session[:code] = Pocket.get_code(redirect_uri: CALLBACK_URL)
+    new_url = Pocket.authorize_url(code: session[:code], redirect_uri: CALLBACK_URL)
+    logger.info "new_url: #{new_url}"
+    logger.info "session: #{session}"
+    redirect new_url
+  end
+
+  get '/oauth/callback' do
+    logger.info 'OAUTH CALLBACK'
+    logger.info "request.url: #{request.url}"
+    logger.info "request.body: #{request.body.read}"
+    result = Pocket.get_result(session[:code], redirect_uri: CALLBACK_URL)
+    session[:access_token] = result['access_token']
+    logger.info result['access_token']
+    logger.info result['username']
+    logger.info session[:access_token]
+    logger.info "session: #{session}"
+    redirect '/'
+  end
+
+  get '/add' do
+    client = Pocket.client(access_token: session[:access_token])
+    info = client.add url: 'http://getpocket.com'
+    "<pre>#{info}</pre>"
+  end
+
+  get '/retrieve' do
+    client = Pocket.client(access_token: session[:access_token])
+    info = client.retrieve(detailType: :complete, count: 1)
+
+    "<pre>#{info}</pre>"
+  end
+end
